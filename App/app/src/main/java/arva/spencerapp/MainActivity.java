3852caab3fac76313c966ac029220af6d0808238
Jack Horsburgh <jackhorsburgh@Jacks-MacBook-Pro.local>
2019-01-29 17:19:07 +0000

    Add bluetooth button and wifi button for connection

--- App/app/src/main/java/arva/spencerapp/MainActivity.java
+++ App/app/src/main/java/arva/spencerapp/MainActivity.java
@@ -1,27 +1,44 @@
 package arva.spencerapp;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button connectRobotBtn;
+    private Button connectBluetoothRobotBtn;
+    private Button connectWifiRobotBtn;
     private Button demoBtn;
+    private final static int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        connectRobotBtn = (Button) findViewById(R.id.connect_btn);
+        connectBluetoothRobotBtn = (Button) findViewById(R.id.connect_bluetooth_btn);
 
-        connectRobotBtn.setOnClickListener(new View.OnClickListener() {
+        connectBluetoothRobotBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Try and connect to robot, then go to Navigation Activity
+                BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+                if (mBluetoothAdapter == null) {
+                    // Device doesn't support Bluetooth
+                    Toast.makeText(getApplicationContext(), "Phone does not support bluetooth", Toast.LENGTH_LONG);
+                    return;
+                }
+
+                if (!mBluetoothAdapter.isEnabled()) {
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                }
+
                 goToNavigationActivity();
             }
         });
