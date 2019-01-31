76cd710c0bb87f0fa3ee5caff4e66beadf6a1c9e
SquidDev <bonzoweb@hotmail.co.uk>
2019-01-31 18:25:23 +0000

    Preparation for remote-controlling the Pi
     - Motor control commands are pushed into an event queue from the
       clients and pulled on a separate async task. The commands will be
       executed until another command is received.
     - All connections are stored in a set, to allow us to send feedback to
       all associated clients.
     - Document the code a little more, to make it more obvious what's going
       on. Well, more obvious to me at least.

--- src/server.py
+++ src/server.py
@@ -1,16 +1,88 @@
-"""A basic server for the demo days"""
+"""A basic server for the demo days."""
 
 import asyncio
 
+class ConnectionManager:
+    """Manages a set of connections, with the ability to send messages to all
+       connected clients.
+
+    """
+    def __init__(self):
+        self.connections = set()
+
+    def add(self, connection):
+        """Add a connection to the manager"""
+        self.connections.add(connection)
+
+    def remove(self, connection):
+        """Remove a connection from the manager"""
+        self.connections.remove(connection)
+
+    def send(self, message):
+        """Send a message to all current connections"""
+        message = (message + "\n").encode()
+        for connection in self.connections:
+            connection.transport.write(message)
+
+class SingleValueQueue:
+    """An asyncronous queue which holds a single value. Simply used to push values
+       between the connections and the motor controller.
+
+    """
+    def __init__(self):
+        self.value = None
+        self.event = asyncio.Event()
+
+    async def pull(self):
+        """Wait for an event to be available and pull it."""
+        await self.event.wait()
+        self.event.clear()
+        return self.value
+
+    def push(self, value):
+        """Push an event to the queue. This will either overwrite the existing event or
+           notify the listening client with it.
+
+        """
+        self.value = value
+        self.event.set()
+
 async def wakeup():
-    """A really ugly hack to allow throwing interrupts within the event loop"""
+    """A really ugly hack to ensure interrupts are thrown within the event loop.
+
+    """
     while True:
         await asyncio.sleep(0.1)
 
-class SpencerServerProtocol(asyncio.Protocol):
-    """The main protocol for Spencer"""
+async def motor_control(queue, manager):
+    """Pulls events from `queue` and executes them on the motors"""
+    # motor.stop_motors()
 
-    def __init__(self):
+    while True:
+        action = await queue.pull()
+        # motor.stop_motors()
+
+        print("Doing " + action)
+        if action[0:3] == "Hey":
+            manager.send("Hey, Hey\n"
+                         "You, You\n"
+                         "I don't like your girlfriend\n"
+                         "No way, no way\n"
+                         "Think you need a new one\n")
+
+class SpencerServerConnection(asyncio.Protocol):
+    """Represents a socket connection to a "spencer client". Namely, a phone running
+       the spencer app.
+
+       This simply registers itself in the connection manager, and assumes all
+       incomming messages are motor controls.
+
+    """
+
+    def __init__(self, queue, manager):
+        self.queue = queue
+        self.manager = manager
+
         self.peername = None
         self.transport = None
         self.buffer = ""
@@ -18,39 +90,53 @@ class SpencerServerProtocol(asyncio.Protocol):
 
     def connection_made(self, transport):
         self.peername = transport.get_extra_info('peername')
-        print('Connection from {}'.format(self.peername))
         self.transport = transport
 
-        self.send('Doing nothing')
+        print('Connection from {}'.format(self.peername))
+        self.manager.add(self)
 
     def connection_lost(self, exc):
         print('Lost connection from {} ({})'.format(self.peername, exc))
+        self.manager.remove(self)
 
     def data_received(self, data):
-        print('Data received')
-
+        # Messages are terminated by a new line, so add to our existing buffer
+        # an split on \n. Any non-terminated data will be stored back in
+        # `self.buffer` again.
         messages = (self.buffer + data.decode()).split("\n")
         self.buffer = messages.pop()
+        # I'm going to confess, the above code is horrible. It works though!
 
         for message in messages:
             self.count += 1
             print('Message received: {}'.format(message))
-            self.send("Received {} messages".format(self.count))
+            self.queue.push(message)
 
     def send(self, message):
-        """Send message to the server"""
+        """Send a `message` to the connected client."""
         self.transport.write((message + "\n").encode())
 
 def _main():
+    """The main entry point of the server"""
+
+    # Create an event loop. This effectively allows us to run multiple functions
+    # at once (namely, the motor controller and server).
     loop = asyncio.get_event_loop()
+    # Motor control statements are pushed into this queue
+    motor_queue = SingleValueQueue()
+    # And we hold all currently connected computers here
+    manager = ConnectionManager()
+
+    # Register our tasks which run along side the server
     loop.create_task(wakeup())
+    loop.create_task(motor_control(motor_queue, manager))
 
-    # Construct the server
-    server = loop.run_until_complete(loop.create_server(SpencerServerProtocol, '0.0.0.0', 1050))
-
+    # Construct the server and run it forever
+    server = loop.run_until_complete(loop.create_server(
+        lambda: SpencerServerConnection(motor_queue, manager),
+        '0.0.0.0', 1050
+    ))
     print('Serving on {}'.format(server.sockets[0].getsockname()))
-
-    # And run it forever
     try:
         loop.run_forever()
     finally:
