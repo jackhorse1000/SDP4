ac9c6215c398e6dc3e56107f1a1d5b01307ef813
SquidDev <bonzoweb@hotmail.co.uk>
2019-01-31 16:47:34 +0000

    Make the server a little more sane WRT the future
     - Use our wacky "\n terminated" encoding as an echo system.
     - Allow it to run on Windows.

--- src/server.py
+++ src/server.py
@@ -1,26 +1,54 @@
-import asyncio
+import asyncio, json
 
-class EchoServerClientProtocol(asyncio.Protocol):
+async def wakeup():
+    """A really ugly hack to allow throwing interrupts within the event loop"""
+    while True:
+        await asyncio.sleep(0.1)
+
+class SpencerServerProtocol(asyncio.Protocol):
+    """The main protocol for Spencer"""
+
+    def __init__(self):
+        self.buffer = ""
+        self.count = 0
+
     def connection_made(self, transport):
         peername = transport.get_extra_info('peername')
         print('Connection from {}'.format(peername))
         self.transport = transport
 
+        self.send('Doing nothing')
+
+    def connection_lost(self, error):
+        peername = transport.get_extra_info('peername')
+        print('Lost connection from {} ({})'.format(peername, error))
+
     def data_received(self, data):
-        message = data.decode()
-        print('Data received: {!r}'.format(message))
-        self.transport.write(data)
+        print('Data received')
+
+        messages = (self.buffer + data.decode()).split("\n")
+        self.buffer = messages.pop()
+
+        for message in messages:
+            self.count += 1
+            print('Message received: {}'.format(message))
+            self.send("Received {} messages" + self.count)
+
+    def send(self, message):
+        self.transport.write((message + "\n").encode())
 
 loop = asyncio.get_event_loop()
-coro = loop.create_server(EchoServerClientProtocol, '0.0.0.0', 1050)
-server = loop.run_until_complete(coro)
+loop.create_task(wakeup())
+
+# Construct the server
+server = loop.run_until_complete(loop.create_server(SpencerServerProtocol, '0.0.0.0', 1050))
 
 print('Serving on {}'.format(server.sockets[0].getsockname()))
+
+# And run it forever
 try:
     loop.run_forever()
-except KeyboardInterrupt:
-    pass
-
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+finally:
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
