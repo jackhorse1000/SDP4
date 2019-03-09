ae8f68132be02d60b00bc4dca87370125d46ef4d
caesar.zhang070497@gmail.com <Zzp970407>
2019-03-09 09:56:34 +0000

    UI beautify

--- App/app/src/main/java/arva/spencerapp/MainActivity.java
+++ App/app/src/main/java/arva/spencerapp/MainActivity.java
@@ -1,11 +1,16 @@
 package arva.spencerapp;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
     private Button connectBluetoothRobotBtn;
     private Button connectWifiRobotBtn;
     private Button demoBtn;
+
+    private TCPClient tcpClient;
+    private TextView statusTxt, connectionTxt;
+    private final String TAG = "MainActivity";
+    private boolean is_connected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +51,11 @@ public class MainActivity extends AppCompatActivity {
                 }).run();
             });
 
-            goToNavigationActivity();
+ //          connectToSpencer();
+
+ //           if(is_connected){
+                goToNavigationActivity();
+ //            }
         });
 
         demoBtn = findViewById(R.id.demo_btn);
@@ -56,6 +70,70 @@ public class MainActivity extends AppCompatActivity {
     private void goToDemoActivity() {
         Intent demoIntent = new Intent(MainActivity.this, DemoActivity.class);
         startActivity(demoIntent);
+    }
+
+/*  private void connectToSpencer() {
+        Handler mainHandler = new Handler(getMainLooper());
+
+        tcpClient = new TCPClient(new TCPClient.MessageCallback() {
+            @Override
+            public void connectionStateChanged(TCPClient.ConnectionState state) {
+                if(state ==null){
+                    showDialog();
+                }
+                mainHandler.post(() -> connectionTxt.setText(state.toString()));
+                String connection_state = state.toString();
+
+
+                if (connection_state.equals("CLOSED")){
+                    mainHandler.post(()-> connectWifiRobotBtn.setClickable(true));
+                }
+
+
+                else if(state == TCPClient.ConnectionState.CONNECTED){
+                    is_connected = true;
+
+                    mainHandler.post(()-> connectWifiRobotBtn.setClickable(false));
+                    Log.d(TAG, "Connection state change: " + state);
+                }
+                else{
+                    mainHandler.post(()-> connectWifiRobotBtn.setClickable(false));
+                    Log.d(TAG, "Connection state change: " + state);
+                }
+
+            }
+
+            @Override
+            public void messageReceived(String message) {
+                mainHandler.post(() -> {
+                    if (message.startsWith("sensor")) {
+//                        updateArray(message); // todo fix
+                    } else {
+                        statusTxt.setText(message);
+                    }
+                });
+                Log.d(TAG, "messaged received: " + message);
+            }
+        });
+
+        TCPClient.EXECUTOR.submit(tcpClient::run);
+    }*/
+
+    private void showDialog(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+
+        builder.setTitle("温馨提示");
+        builder.setMessage("天冷多加衣！");
+        builder.setPositiveButton("我知道了",
+            new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+        AlertDialog dialog=builder.create();
+        dialog.show();
+
     }
 
 }
