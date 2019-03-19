a6151d6e82bb7f8f6b7681c2092f67cffe31883a
jackhorse1000 <jackhorse1000@gmail.com>
2019-03-19 10:56:22 +0000

    Start downstairs alignment

--- src/climb.py
+++ src/climb.py
@@ -69,3 +69,53 @@ class ClimbController:
 
         LOG.error("Stopping due to no longer moving.")
         return False
+
+    async def downstairs_find_wall(self) -> bool:
+        """Attempt to find a wall and align itself against it.
+          Returns when we are reasonable aligned to the step in front."""
+        left, right = self.sensors.front_dist_0, self.sensors.front_dist_1
+        failure = 0
+        LOG.info("Attempting to align against a wall. This is gonna go badly.")
+        while self.sensors.get_moving():
+            if failure > 10:
+                LOG.error("front_up aborting due to too many failed reads")
+                return False
+
+            # If only one is valid, rotate towards the valid sensor
+            if left.valid and left.value >= 10 and not right.valid:
+                control.turn_left()
+            elif right.valid and right.value >= 10 and not left.valid:
+                control.turn_right()
+
+            # If neither are valid, then drive forward.
+            elif not left.valid or not right.valid:
+                # control.forward()
+                # TODO: We could be fine or too close to the wall, but depends when we call
+                # This should not be the case
+            else:
+                failure = 0
+                distance = min(left.value, right.value)
+                delta = left.value - right.value
+                LOG.debug("Distance=%f, delta=%f", distance, delta)
+
+                # If we're a long way away, continue to move forward
+                if distance >= 25:
+                    # control.forward()
+                    # TODO: We are too far from the wall no point aligning
+                # Attempt to align against the wall
+                elif delta > 3:
+                    control.turn_right()
+                elif delta < -3:
+                    control.turn_left()
+                elif distance <= 6:
+                    control.stop()
+                    return True
+
+                # We're now aligned, but still a way away - move closer!
+                else:
+                    control.forward()
+
+            await asyncio.sleep(SLEEP)
+
+        LOG.error("Stopping due to no longer moving.")
+        return False
